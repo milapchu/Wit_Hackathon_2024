61fb8fe95b3d06791a8ccac7742158a7a06c4ed9
XINYU DONG <144611334+Clariya@users.noreply.github.com>
2024-09-18 21:51:11 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from website import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy import Enum
+
 
 # Association table to link users and groups (many-to-many relationship)
 user_group = db.Table(
@@ -13,6 +15,8 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    status =db.Column(Enum('Done','Not Done'),name = 'task_status', nullable = False, default = 'Not Done')
+    frequency = db.Column(Enum('Daily','Weekly','Fortnightly','Monthly'), name = 'task_frenquency', nullable = False, default = 'Monthly')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one user can have many tasks
     group_id  = db.Column(db.Integer, db.ForeignKey('group.id')) # one group can have many tasks
 
