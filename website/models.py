b3396c3f5392e7367193b12b0842f8d25953f0a5
Mila PChu <uchu0676@uni.sydney.edu.au>
2024-09-18 15:12:06 +1000

    initial implementation

---
+++ website/models.py
@@ -0,0 +1,32 @@
+from website import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+# Association table to link users and groups (many-to-many relationship)
+user_group = db.Table(
+    'user_group',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True)
+)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # one user can have many tasks
+    group_id  = db.Column(db.Integer, db.ForeignKey('group.id')) # one group can have many tasks
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    groups = db.relationship('Group', secondary=user_group, backref=db.backref('members', lazy=True)) # Many-to-many with groups
+    tasks = db.relationship('Task') #all tasks of a user
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_name  = db.Column(db.String(100), nullable=False)
+    tasks = db.relationship('Task') #all tasks of a user
+
---
+++ website/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, flash, request
+from flask_login import login_required, current_user
+from website import db
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
