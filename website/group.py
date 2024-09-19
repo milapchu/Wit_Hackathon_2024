91af2b9301c5c6cafc8e4cd315ebf17ada2cf0df
XINYU DONG <144611334+Clariya@users.noreply.github.com>
2024-09-19 22:10:14 +1000

    join_group changes

--- website/group.py
+++ website/group.py
@@ -1,7 +1,8 @@
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from website import db 
-from .models import Task, Group 
+from .models import Task, Group , user_group
 
 group = Blueprint('group', __name__)
 
@@ -28,4 +29,40 @@ def create_group():
     
     return render_template("create_group.html", user=current_user)
 
+@group.route('/join_group', methods=['GET', 'POST'])
+@login_required
+def join_group():
+    if request.method == 'POST':
+        group_name = request.form.get('group_name')
+        
+        group = Group.query.filter_by(group_name=group_name).first()
+        
+        if group:
+            is_member = db.session.execute(
+                db.select(user_group).where(
+                    (user_group.c.user_id == current_user.id) &
+                    (user_group.c.group_id == group.id)
+                )
+            ).fetchone()
+            
+            if is_member:
+                existing_task = Task.query.filter_by(user_id=current_user.id, group_id=group.id).first()
+                
+                if existing_task:
+                    flash('You are already in the group. Try to add more tasks!', category='error')
+                else:
+                    flash('You are in the group, let us have some tasks!', category='success')
+                
+                return redirect(url_for('task.create_task'))
+            else:
+                current_user.groups.append(group)
+                db.session.commit()
+                flash('You have successfully joined the group!', category='success')
+                return redirect(url_for('task.create_task'))
+        else:
+            flash('Group does not exist. Please enter a valid group name.', category='error')
+            return redirect(url_for('group.join_group'))
+    
+    # For GET requests, render the join group page
+    return render_template('join_group.html', user=current_user)
 
